4dc9ceebad747cb4315f08523975280b2132099d
Damon Bogich <damonbogich@gmail.com>
2021-02-17 13:03:54 -0700

    disabled button on home page when no car is set to state

--- src/components/CarOption.js
+++ src/components/CarOption.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { connect } from "react-redux";
 import {setCar} from '../actions';
 import {Link} from 'react-router-dom';
 
 const CarOption = (props) => {
+    const [disabled, setDisabled] = useState(true)
 
+    console.log(props.car, 'car option props')
+    
     const setCar = (e) => {
-        e.preventDefault();
         props.setCar(props.item);
+        setDisabled(!disabled)
     };
-
+        
     return (
         <div className="boxes">
             <div className="box">
                 <h2 style={{color: 'white'}}>{props.item.name}</h2>
                 <img style={{color: 'white'}} src={props.item.image} alt={props.item.name} />
                 <button onClick={setCar}>Choose</button> 
-                <Link to="/car"><button>Go to car</button></Link> 
+                <Link to="/car"><button disabled={disabled}>Go to car</button></Link> 
             </div>
         </div>
         
